ad14774cf31560127c5780468ed77b121597d2e1
ibzzsfw <tap.pt33@gmail.com>
2023-10-29 21:11:04 +0700

    feat: ext simple

---
+++ gaimonqa/simple/SimpleExtension.py
@@ -0,0 +1,16 @@
+from gaimon.core.Extension import Extension
+
+class SimpleExtension (Extension) :
+	def __init__(self, resourcePath:str, configPath:str):
+		self.ID = "simple"
+		self.name = "Simple"
+		self.path = self.getPath(__file__)
+		super().__init__(resourcePath, configPath)
+
+	# NOTE This method will be called by initialize module for first time.
+	async def initialize(self, isCopy:bool=True, isForce:bool=False, isSetLocalConfig:bool=True) :
+		await super().initialize(isCopy, isForce)
+	
+	# NOTE This method will be called by start Gaimon server.
+	async def load(self, application) :
+		await super().load(application)
