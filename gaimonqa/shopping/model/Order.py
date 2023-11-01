da62f296d6653b418dfa1f9352af1e152bcc3398
Grapeka <chotwilaiwan@gmail.com>
2023-11-01 22:05:51 +0700

    fix: add children for shopping model

--- gaimonqa/shopping/model/Order.py
+++ gaimonqa/shopping/model/Order.py
@@ -1,7 +1,9 @@
 from xerial.Record import Record
 from xerial.DateTimeColumn import DateTimeColumn
 from xerial.IntegerColumn import IntegerColumn
+from xerial.Children import Children
 
 class Order (Record):
     date = DateTimeColumn() # Timestamp 
     customer = IntegerColumn(foreignKey="Customer.id")
+    orderItem = Children("OrderItem.id")
