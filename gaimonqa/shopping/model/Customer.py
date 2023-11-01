fbfc386185208440fe8046e8fc6bf00697362c64
Grapeka <chotwilaiwan@gmail.com>
2023-11-01 22:05:51 +0700

    fix: add children for shopping model

--- gaimonqa/shopping/model/Customer.py
+++ gaimonqa/shopping/model/Customer.py
@@ -1,6 +1,8 @@
 from xerial.Record import Record
 from xerial.StringColumn import StringColumn
+from xerial.Children import Children
 
 class Customer (Record):
     name = StringColumn()
     email = StringColumn()
+    order = Children("Order.id")
--- gaimonqa/shopping/model/Product.py
+++ gaimonqa/shopping/model/Product.py
@@ -1,7 +1,9 @@
 from xerial.Record import Record
 from xerial.StringColumn import StringColumn
 from xerial.FloatColumn import FloatColumn
+from xerial.Children import Children
 
 class Product (Record):
     name = StringColumn()
     price = FloatColumn(precision=2, length=10)
+    orderItem = Children("OrderItem.id")
