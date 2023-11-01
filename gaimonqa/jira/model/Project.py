b15f58a0b790e0c82d0214a98fedae8d0048b909
ibzzsfw <tap.pt33@gmail.com>
2023-11-01 22:20:13 +0700

    Revert "feat: add jira model"
    This reverts commit 8e9cc38aa7910ed27ba2b4426f3f3c2a5fbd5786.

--- gaimonqa/jira/model/Project.py
+++
@@ -1,9 +0,0 @@
-from xerial.Record import Record
-from xerial.StringColumn import StringColumn
-from xerial.IntegerColumn import IntegerColumn
-from xerial.Children import Children
-
-class Project (Record):
-    name = StringColumn()
-    user = IntegerColumn(foreignKey="User.id")
-    task = Children("Task.id")
